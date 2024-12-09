9216f190110f2ec54a50001b8d17eccabe4d9e8c
andrey kassaev <andrey@kassaev.com>
2024-12-09 11:58:01 +0400

    renamed (Task/Detail)ScreenFragment

--- app/src/main/java/com/kassaev/planner/screen/CalendarMonthScreenFragment.kt
+++ app/src/main/java/com/kassaev/planner/screen/CalendarMonthScreenFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.kassaev.planner.R
 import com.kassaev.planner.databinding.FragmentCalendarMonthScreenBinding
 
 class CalendarMonthScreenFragment : Fragment() {
@@ -21,6 +23,6 @@ class CalendarMonthScreenFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        findNavController().navigate(R.id.taskDetailScreenFragment)
     }
 }
--- app/src/main/java/com/kassaev/planner/screen/TaskDetailScreenFragment.kt
+++ app/src/main/java/com/kassaev/planner/screen/TaskDetailScreenFragment.kt
@@ -5,17 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.kassaev.planner.databinding.FragmentDetailTaskScreenBinding
+import com.kassaev.planner.databinding.FragmentTaskDetailScreenBinding
 
-class DetailTaskScreenFragment : Fragment() {
+class TaskDetailScreenFragment : Fragment() {
 
-    private lateinit var binding: FragmentDetailTaskScreenBinding
+    private lateinit var binding: FragmentTaskDetailScreenBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentDetailTaskScreenBinding.inflate(inflater, container, false)
+        binding = FragmentTaskDetailScreenBinding.inflate(inflater, container, false)
         return binding.root
     }
 
