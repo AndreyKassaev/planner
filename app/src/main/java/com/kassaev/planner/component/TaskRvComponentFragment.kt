134b4fd8278e5fafe9aa6c7251d62949b77277b1
andrey kassaev <andrey@kassaev.com>
2024-12-10 08:58:02 +0400

    created task rv list generator

--- app/src/main/java/com/kassaev/planner/component/TaskRvComponentFragment.kt
+++ app/src/main/java/com/kassaev/planner/component/TaskRvComponentFragment.kt
@@ -8,6 +8,12 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.kassaev.planner.adapter.TaskAdapter
 import com.kassaev.planner.databinding.FragmentTaskRvComponentBinding
+import com.kassaev.planner.model.Task
+import com.kassaev.planner.model.TaskRvItem
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 
 class TaskRvComponentFragment : Fragment() {
 
@@ -26,7 +32,47 @@ class TaskRvComponentFragment : Fragment() {
 
         binding.taskRV.apply {
             layoutManager = LinearLayoutManager(requireContext())
-            adapter = TaskAdapter(emptyList())
+            adapter = TaskAdapter(MockSchedule.getTaskRvList())
         }
+        MockSchedule.getTaskRvList()
+            .forEach { hour ->
+                println("${hour.hour} - ${hour.taskList.size}")
+            }
+    }
+}
+
+object MockSchedule {
+
+    private val mockTaskList = listOf(
+        Task(
+            id = "1",
+            dateStart = "1733743989960",
+            dateFinish = "2",
+            name = "singleTask",
+            description = "desc"
+        )
+    )
+
+
+    fun getTaskRvList(taskList: List<Task> = mockTaskList): List<TaskRvItem> {
+        val hourList = MutableList(24) {
+            TaskRvItem(
+                hour = it.toString(),
+                taskList = emptyList()
+            )
+        }
+
+        taskList.forEach { task ->
+            val index = timestampToLocalHours(task.dateStart).toInt()
+            hourList[index] = hourList[index].copy(taskList = hourList[index].taskList + task)
+        }
+        return hourList
+    }
+
+    fun timestampToLocalHours(timestamp: String): String {
+        val date = Date(timestamp.toLong())
+        val sdf = SimpleDateFormat("HH", Locale("ru"))
+        sdf.timeZone = TimeZone.getDefault()
+        return sdf.format(date)
     }
 }
