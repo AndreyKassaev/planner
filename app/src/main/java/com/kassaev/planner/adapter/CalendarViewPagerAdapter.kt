4dcd0936b9773679edd07c29288735ab228da686
andrey kassaev <andrey@kassaev.com>
2024-12-17 12:59:27 +0400

    highlighted weekends

--- app/src/main/java/com/kassaev/planner/adapter/CalendarViewPagerAdapter.kt
+++ app/src/main/java/com/kassaev/planner/adapter/CalendarViewPagerAdapter.kt
@@ -1,17 +1,20 @@
 package com.kassaev.planner.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
@@ -21,10 +24,9 @@ import com.kassaev.planner.R
 import com.kassaev.planner.databinding.FragmentCalendarMonthComponentBinding
 import com.kassaev.planner.model.CalendarDate
 import com.kassaev.planner.model.Month
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
-import java.util.Locale
+import com.kassaev.planner.util.getDay
+import com.kassaev.planner.util.getMonthResourceId
+import com.kassaev.planner.util.getYear
 
 class CalendarViewPagerAdapter(
     private val items: List<Month>
@@ -37,67 +39,31 @@ class CalendarViewPagerAdapter(
             binding.calendarYear.text = getYear(month)
             binding.calendarMonth.text = getString(binding.root.context, getMonthResourceId(month))
             binding.composeView.setContent {
+                val indexOffset = month.previousMonthLastWeekDateList.size
                 LazyVerticalGrid(
-                    columns = GridCells.Fixed(7), // 7 columns
+                    columns = GridCells.Fixed(7),
                     modifier = Modifier.fillMaxSize(),
                     contentPadding = PaddingValues(16.dp)
                 ) {
-                    month.previousMonthLastWeekDateList.forEach { date ->
+                    month.previousMonthLastWeekDateList.forEachIndexed { index, date ->
                         item {
-                            Text(
-                                modifier = Modifier
-                                    .padding(8.dp)
-                                    .alpha(0.5F),
-                                text = getDay(date),
-                                fontSize = 24.sp,
-                            )
+                            GridItem(index = index, date = date)
                         }
                     }
-                    month.currentMonthDateList.forEach { date ->
+                    month.currentMonthDateList.forEachIndexed { index, date ->
                         item {
-                            Text(
-                                modifier = Modifier.padding(8.dp),
-                                text = getDay(date),
-                                fontSize = 24.sp,
-                            )
+                            GridItem(index = index + indexOffset, date = date, isCurrent = true)
                         }
                     }
-                    month.followingMonthFirstWeekDateList.forEach { date ->
+                    month.followingMonthFirstWeekDateList.forEachIndexed { index, date ->
                         item {
-                            Text(
-                                modifier = Modifier
-                                    .padding(8.dp)
-                                    .alpha(0.5F),
-                                text = getDay(date),
-                                fontSize = 24.sp,
-                            )
+                            GridItem(index = index, date = date)
                         }
                     }
                 }
             }
         }
     }
-
-    private fun getMonthResourceId(month: Month) =
-        when (month.currentMonthDateList.first().date.split("-")[1].toInt()) {
-            1 -> R.string.january
-            2 -> R.string.february
-            3 -> R.string.march
-            4 -> R.string.april
-            5 -> R.string.may
-            6 -> R.string.june
-            7 -> R.string.july
-            8 -> R.string.august
-            9 -> R.string.september
-            10 -> R.string.october
-            11 -> R.string.november
-            12 -> R.string.december
-            else -> R.string.unknown
-        }
-
-
-    private fun getYear(month: Month) =
-        month.currentMonthDateList.first().date.split("-").first()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = FragmentCalendarMonthComponentBinding.inflate(
@@ -113,33 +79,27 @@ class CalendarViewPagerAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(items[position])
     }
+}
 
-    private fun getCalendarDateTextView(
-        date: CalendarDate,
-        isCurrent: Boolean = false,
-        context: Context
-    ): TextView {
-        val currentDate = Calendar.getInstance().time
-        val textView = TextView(context)
-        textView.setPadding(32, 32, 32, 32)
-        textView.textSize = 24F
-        textView.alpha = if (isCurrent) 1F else 0.25F
-        textView.text = getDay(date)
-        textView.setOnClickListener {
-            println(date.date)
-        }
-        if (formatDateWithoutTime(SimpleDateFormat("yyyy-MM-dd").parse(date.date)) == formatDateWithoutTime(
-                currentDate
-            )
-        ) textView.setTextColor(
-            ContextCompat.getColor(context, R.color.calendar)
-        )
-        return textView
+@Composable
+fun GridItem(
+    index: Int,
+    date: CalendarDate,
+    isCurrent: Boolean = false
+) {
+    val context = LocalContext.current
+    val columnIndex = index % 7
+    val backgroundColor = when (columnIndex) {
+        5, 6 -> Color(ContextCompat.getColor(context, R.color.highlightColor))
+        else -> Color.Transparent
     }
-
-    private fun getDay(date: CalendarDate) = date.date.split("-").last().toInt().toString()
-
-    private fun formatDateWithoutTime(date: Date) =
-        SimpleDateFormat("dd-MM-yyyy", Locale("ru")).format(date)
-
+    Text(
+        modifier = Modifier
+            .padding(8.dp)
+            .alpha(if (isCurrent) 1F else 0.5F)
+            .background(color = if (isCurrent) backgroundColor else Color.Transparent),
+        text = getDay(date),
+        fontSize = 24.sp,
+        textAlign = TextAlign.Center
+    )
 }
---
+++ app/src/main/java/com/kassaev/planner/util/Util.kt
@@ -0,0 +1,60 @@
+package com.kassaev.planner.util
+
+import android.content.Context
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.kassaev.planner.R
+import com.kassaev.planner.model.CalendarDate
+import com.kassaev.planner.model.Month
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+
+fun getCalendarDateTextView(
+    date: CalendarDate,
+    isCurrent: Boolean = false,
+    context: Context
+): TextView {
+    val currentDate = Calendar.getInstance().time
+    val textView = TextView(context)
+    textView.setPadding(32, 32, 32, 32)
+    textView.textSize = 24F
+    textView.alpha = if (isCurrent) 1F else 0.25F
+    textView.text = getDay(date)
+    textView.setOnClickListener {
+        println(date.date)
+    }
+    if (formatDateWithoutTime(SimpleDateFormat("yyyy-MM-dd").parse(date.date)) == formatDateWithoutTime(
+            currentDate
+        )
+    ) textView.setTextColor(
+        ContextCompat.getColor(context, R.color.calendar)
+    )
+    return textView
+}
+
+fun getDay(date: CalendarDate) = date.date.split("-").last().toInt().toString()
+
+fun formatDateWithoutTime(date: Date) =
+    SimpleDateFormat("dd-MM-yyyy", Locale("ru")).format(date)
+
+fun getMonthResourceId(month: Month) =
+    when (month.currentMonthDateList.first().date.split("-")[1].toInt()) {
+        1 -> R.string.january
+        2 -> R.string.february
+        3 -> R.string.march
+        4 -> R.string.april
+        5 -> R.string.may
+        6 -> R.string.june
+        7 -> R.string.july
+        8 -> R.string.august
+        9 -> R.string.september
+        10 -> R.string.october
+        11 -> R.string.november
+        12 -> R.string.december
+        else -> R.string.unknown
+    }
+
+fun getYear(month: Month) =
+    month.currentMonthDateList.first().date.split("-").first()
