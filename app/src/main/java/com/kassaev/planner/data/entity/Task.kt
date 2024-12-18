53f0097b5fe6c521a32d8065c3ba2812c1fe4244
Andrey Kassaev <andrey@kassaev.com>
2024-12-18 19:49:39 +0400

    fixed db,entity

--- app/src/main/java/com/kassaev/planner/data/entity/Task.kt
+++ app/src/main/java/com/kassaev/planner/data/entity/Task.kt
@@ -1,9 +1,11 @@
 package com.kassaev.planner.data.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
 data class Task(
+    @PrimaryKey
     val id: Long,
     val dateStart: Long,
     val dateFinish: Long,
